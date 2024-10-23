08bedaa30d82a2c8da6a1af20bf24f38889de133
Molkobahn <stoffeosterhof@gmail.com>
2024-10-23 16:27:19 +0200

    made FPS

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
 
+
 def main():
 	pygame.init()
-	print("Starting asteroids!")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return 
-		pygame.Surface.fill(screen, 000000, rect = None)
+		
+		screen.fill("black")
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+
 
 if __name__=="__main__":
 	main()
