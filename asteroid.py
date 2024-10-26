014b078b220aa1d4399b994a6dc9ad66d1b57648
Molkobahn <stoffeosterhof@gmail.com>
2024-10-26 10:16:17 +0200

    made asteroid split

--- asteroid.py
+++ asteroid.py
@@ -3,6 +3,7 @@ import random
 from constants import *
 from circleshape import CircleShape
 
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -12,3 +13,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+        
+        new_vector_1 = self.velocity.rotate(random_angle)
+        new_vector_2 = self.velocity.rotate(-random_angle)
+        
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1.velocity = new_vector_1 * 1.2
+        new_asteroid_2.velocity = new_vector_2 * 1.2
